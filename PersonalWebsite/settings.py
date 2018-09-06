ca9cfcee90da3025b168dff4e7f32a14ea968141
Attreya <attreya01@gmail.com>
2018-09-06 12:24:29 +0530

    Finished Project on Local

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
---
+++ venv/Lib/site-packages/whitenoise/__init__.py
@@ -0,0 +1,5 @@
+from .base import WhiteNoise
+
+__version__ = '4.0'
+
+__all__ = ['WhiteNoise']
